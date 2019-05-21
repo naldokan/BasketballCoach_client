2fbff1d760e9afac113eeba1b58c3d7d7fe984a1
voyager-gold <eugenemathew93@gmail.com>
2019-05-20 19:09:15 -0700

    history

--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -3,14 +3,8 @@ import { withRouter } from "react-router";
 import { compose } from 'redux'
 
 import cx from 'classnames'
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavLink
-} from 'reactstrap';
+import Calendar from 'react-calendar'
+import { Row, Col, Table } from 'reactstrap';
 
 import './styles.scss';
 
@@ -20,13 +14,80 @@ class History extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      date: new Date()
     }
   }
+
+  onDateChange = () => {}
 
   render() {
     return (
       <>
-        Dashboard
+        <Row className='history-pane'>
+          <Col className='col-12 col-md-6 mb-5'>
+            <Calendar
+              calendarType="US"
+              onChange={this.onDateChange}
+              value={this.state.date}
+            />
+          </Col>
+          <Col className='col-12 col-md-6'>
+            <Table striped borderless>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Release Angle</th>
+                  <th>Release Time</th>
+                  <th>Leg Angle</th>
+                  <th>Vertical</th>
+                  <th>Speed</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>34</td>
+                  <td>26</td>
+                  <td>90</td>
+                  <td>10</td>
+                  <td>6</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>34</td>
+                  <td>26</td>
+                  <td>90</td>
+                  <td>10</td>
+                  <td>6</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>34</td>
+                  <td>26</td>
+                  <td>90</td>
+                  <td>10</td>
+                  <td>6</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>34</td>
+                  <td>26</td>
+                  <td>90</td>
+                  <td>10</td>
+                  <td>6</td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>34</td>
+                  <td>26</td>
+                  <td>90</td>
+                  <td>10</td>
+                  <td>6</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
       </>
     );
   }
